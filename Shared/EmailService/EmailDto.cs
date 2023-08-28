0a17b529ca4e482aba022fee72cdb0b601d0c6ff
utsav.basyal <45966409+utsabbasyal12@users.noreply.github.com>
2023-08-28 15:33:49 +0545

    Added Email Services

---
+++ Shared/EmailService/EmailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.EmailService
+{
+    public class EmailDto
+    {
+        public List<string>? Recipients { get; set; }
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
---
+++ Shared/EmailService/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace Shared.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendEmail(EmailDto request, List<string> recipents)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailSettings")["EmailUsername"]));
+            foreach (var recipent in recipents)
+            {
+                email.To.Add(MailboxAddress.Parse(recipent));
+            }
+            //email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_configuration.GetSection("EmailSettings")["EmailHost"], 587, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate(_configuration.GetSection("EmailSettings")["EmailUsername"], _configuration.GetSection("EmailSettings")["EmailPassword"]);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
